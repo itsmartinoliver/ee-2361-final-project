589eb9b0484c5588362c8d22f9a9c3685f749ff2
itsmartinoliver <itsmartinoliver@gmail.com>
2026-03-29 11:47:24 -0500

    all files

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ee-2361-final-project.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ee-2361-final-project.X.production.hex
